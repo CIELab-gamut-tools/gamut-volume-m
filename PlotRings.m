68c67831aa9f0f0f8fe4fc3597f7a30aa84c5ebb
Euan Smith <euan1971@yahoo.co.uk>
2021-09-24 12:59:17 +0100

    intersection added with (I hope) sensible options

--- PlotRings.m
+++ PlotRings.m
@@ -53,23 +53,25 @@ function PlotRings(gamut, varargin)
 %   RingLine          - The linespec of all the gamut rings.
 %                       ['k' (default) | char] 
 %
-%   RefLine           - The linespec of the first reference gamut ring.
+%   RefLine           - The linespec of the first outer reference gamut ring.
 %                       ['--k' (default) | char] 
 %
-%   Ref2Line          - The linespec of the second reference gamut ring.
+%   Ref2Line          - The linespec of the second outer reference gamut ring.
 %                       [':k' (default) | char] 
 %
-%   RingReference     - The single reference ring does not resolve the 
-%                       gamut by luminance, it is just an indication of
-%                       overall volume against hue.  This option provides
-%                       two ways to visualise the comparison to the
-%                       reference per lightness ring. 'intersection' plots
-%                       a dotted line per ring which is the intersection of
-%                       the test and (first) reference gamuts. 'ref' plots
-%                       a dotted line per ring which is the volume of the
-%                       reference gamut in the requisite lightness range,
-%                       where it is less than the the test gamut.
-%                       ['none' (default) | 'intersection' | 'ref'] 
+%   IntersectionPlot  - Specify if, when a test and reference gamut is
+%                       specified, the intersection plot should be rendered
+%                       rather than the standard rings plot.
+%                       [true | false (default)]
+%
+%   IntersectionLine  - The linespec used for plotting the intersection
+%                       ['' (default) | char]
+%
+%   IntersectGamut    - If true the test gamut is intersected with the
+%                       (first or only) reference gamut before display.  If
+%                       the intersection plot is shown, this is
+%                       automatically true.
+%                       [true | false (default)]
 %
 % +Gamut Bands
 %   ShowBands         - The Bands are the coloured sections between the
@@ -87,6 +89,27 @@ function PlotRings(gamut, varargin)
 %                       match the number of bands defined in LRings
 %                       *including the outer L*=100 band*
 %                       [[20 90] (default) | vector]
+%
+%   BandHue           - The hue of the bands.  This can either match the
+%                       hue angle of the chart ('match'), or be a fixed
+%                       hue. The hue is defined as 0 for a*=1, b*=0 and 90
+%                       for a*=0, b*=1.
+%                       [0-359 | 'match' (default)]
+%
+%   ShowRefBands      - Display bands containing the reference and test
+%                       gamuts. Where the reference band is not overlapped
+%                       by the test band, the colour shown will accord with
+%                       the RefBand settings.
+%                       [true (default) | false]
+%
+%   RefBandChroma     - As BandChroma, applied to the Ref Band, if shown.
+%                       [0 (default) | positive scalar]
+%
+%   RefBandLs         - As BandLs, applied to the Ref Band, if shown.
+%                       [[30 98] (default) | vector]
+%
+%   RefBandHue        - As BandHue, applied to the Ref Band, if shown.
+%                       [0-359 | 'match' (default)]
 %
 % +Decorations
 %   CentMark          - The linespec of the centre marker.  An empty array
@@ -133,6 +156,8 @@ function PlotRings(gamut, varargin)
 %
 % https://github.com/CIELab-gamut-tools/gamut-volume-m
 
+
+
 %import all of the functions in the +CIEtools folder
 try
     import CIEtools.*;
@@ -169,13 +194,20 @@ addParameter(p,'LLabelColors','default',@(x) ischar(x) || isstring(x) || (isnume
 addParameter(p,'RingLine','k',@ischar);
 addParameter(p,'RefLine','--k',@ischar);
 addParameter(p,'Ref2Line',':k',@ischar);
+addParameter(p,'IntersectionLine','',@ischar);
 addParameter(p,'RingReference','none',@(x) any(validatestring(x,{'none','intersection','ref'},'PlotRings')));
-
+addParameter(p,'IntersectGamuts',false,@islogical);
+addParameter(p,'IntersectionPlot',false,@islogical);
 
 %=====Gamut Band Format=====
 addParameter(p,'ShowBands',true,@islogical);
 addParameter(p,'BandChroma',50,@isnumeric);
 addParameter(p,'BandLs',[20 90],@isnumeric);
+addParameter(p,'BandHue','match',@(x) isnumeric(x) || any(validatestring(x,{'match'},'PlotRings')));
+addParameter(p,'ShowRefBands',true,@islogical);
+addParameter(p,'RefBandChroma',0,@isnumeric);
+addParameter(p,'RefBandLs',[30 98],@isnumeric);
+addParameter(p,'RefBandHue','match',@(x) isnumeric(x) || any(validatestring(x,{'match'},'PlotRings')));
 
 %=====Chart Decorations=====
 addParameter(p,'CentMark','+k',@ischar);
@@ -183,11 +215,11 @@ addParameter(p,'CentMarkSize',20,@isscalar);
 addParameter(p,'ChromaRings',0,@isnumeric);
 
 %=====Primary colour indicators=====
-addParameter(p,'PrimaryChroma',950,@isnumeric);
+addParameter(p,'PrimaryChroma',1150,@isnumeric);
 addParameter(p,'PrimaryOrigin','centre',@(x) any(validatestring(x,{'centre','center','ring'},'PlotRings')));
-addParameter(p,'RefPrimaryChroma',1000,@isnumeric);
+addParameter(p,'RefPrimaryChroma',1200,@isnumeric);
 addParameter(p,'RefPrimaryOrigin','ring',@(x) any(validatestring(x,{'centre','center','ring'},'PlotRings')));
-addParameter(p,'Primaries','rgb',@(x) any(validatestring(x,{'none','rgb','all'},'PlotRings')));
+addParameter(p,'Primaries','none',@(x) any(validatestring(x,{'none','rgb','all'},'PlotRings')));
 addParameter(p,'RefPrimaries','none',@(x) any(validatestring(x,{'none','rgb','all'},'PlotRings')));
 
 %=====Scatter Point Data=====
@@ -199,10 +231,31 @@ parse(p,gamut,varargin{:});
 % extract a few values from the parsed results
 refgamut = p.Results.ref;
 refgamut2 = p.Results.ref2;
-lrings = p.Results.LRings;
+
+% if required, calculate the intersected gamut
+intersectionPlot = p.Results.IntersectionPlot && validGamut(refgamut);
+intersectGamuts = p.Results.IntersectGamuts || intersectionPlot;
+if intersectGamuts && validGamut(refgamut)
+  testGamut = IntersectGamuts(gamut, refgamut);
+  origVol = GetVolume(gamut);
+else
+  testGamut = gamut;
+  origVol = [];
+end
 
 %calculate the main set of gamut rings
-[x,y,~,vol] = calcRings(gamut,lrings);
+lrings = p.Results.LRings;
+if intersectionPlot
+  rings = ringsBase(testGamut,lrings, refgamut);
+  [testX, testY, testVol] = calcSubRings(rings, testGamut);
+  [refX, refY, refVol] = calcSubRings(rings, refgamut);
+else
+  rings = ringsBase(testGamut,lrings);
+  testX = rings.x(2:end,:);
+  testY = rings.y(2:end,:);
+  testVol = rings.vol;
+  if validGamut(refgamut), refVol = GetVolume(refgamut); end
+end
 
 axes(p.Results.Axes);
 if (p.Results.ClearAxes), cla; end
@@ -211,23 +264,35 @@ hold on;
 
 % ================== Main figure ===================== %
 
+%add the reference coloured bands, if specified
+if (intersectionPlot && p.Results.ShowRefBands)
+    %fill in the colours
+    chroma=p.Results.RefBandChroma;
+    N=size(rings.x,1)-1;
+    ls=normRange(p.Results.RefBandLs,N);
+    lim=size(rings.x,2)*2;
+    TRI=[1:lim; 2:lim 1; 3:lim 1:2]; 
+    for n=1:N
+        xc=reshape([rings.x(n,:);refX(n,:)],[],1);
+        yc=reshape([rings.y(n,:);refY(n,:)],[],1);
+        rgb=lab2srgb([ls(n)+zeros(size(xc)), chroma*kron(rings.ux(:),[1;1]), chroma*kron(rings.uy(:),[1;1])])/255;
+        noLegend(patch('XData',xc(TRI),'YData',yc(TRI),'EdgeColor','none',...
+          'FaceVertexCData',rgb(TRI,:),'FaceColor','interp'));
+    end
+end
+
 %add the coloured bands, if specified
 if (p.Results.ShowBands)
     %fill in the colours
     chroma=p.Results.BandChroma;
-    ls=p.Results.BandLs;
-    N=size(x,1)-1;
-    if numel(ls)==2
-      ls=ls(1):(ls(2)-ls(1))/N:ls(2);
-    end
-    r=max(1,sqrt(x.^2+y.^2));
-    lim=size(x,2)*2;
+    N=size(rings.x,1)-1;
+    ls=normRange(p.Results.BandLs,N);
+    lim=size(rings.x,2)*2;
     TRI=[1:lim; 2:lim 1; 3:lim 1:2]; 
     for n=1:N
-        xc=reshape(x(n:n+1,:),[],1);
-        yc=reshape(y(n:n+1,:),[],1);
-        rc=reshape(r(n:n+1,:),[],1);
-        rgb=lab2srgb([ls(n)+rc*0, chroma*xc./rc, chroma*yc./rc])/255;
+        xc=reshape([rings.x(n,:);testX(n,:)],[],1);
+        yc=reshape([rings.y(n,:);testY(n,:)],[],1);
+        rgb=lab2srgb([ls(n)+zeros(size(xc)), chroma*kron(rings.ux(:),[1;1]), chroma*kron(rings.uy(:),[1;1])])/255;
         noLegend(patch('XData',xc(TRI),'YData',yc(TRI),'EdgeColor','none',...
           'FaceVertexCData',rgb(TRI,:),'FaceColor','interp'));
     end
@@ -235,27 +300,14 @@ end
 
 %plot the figure
 if ~isempty(p.Results.RingLine)
-    h=plot(x(:,[1:end 1])',y(:,[1:end 1])',p.Results.RingLine);
+    h=plot(rings.x(:,[1:end 1])',rings.y(:,[1:end 1])',p.Results.RingLine);
     noLegend(h(1:end-1));
 end
 
-%add the ring references, if specified
-ringRef = validatestring(p.Results.RingReference,{'none','intersection','ref'});
-if ~strcmp(ringRef,'none')
-    if strcmp(ringRef,'intersection')
-        ringref = IntersectGamuts(gamut, refgamut);
-    else
-        ringref = refgamut;
-    end
-    [xi,yi]=calcRings(ringref,lrings);
-    ri=xi.^2+yi.^2;
-    rg=x.^2+y.^2;
-    rsc=min(1,sqrt((ri(2:end,:)-ri(1:end-1,:)+rg(1:end-1,:))./rg(2:end,:)));
-    xd=x(2:end,:).*rsc;
-    yd=y(2:end,:).*rsc;
-    noLegend(plot(xd(:,[1:end 1])',yd(:,[1:end 1])',p.Results.RefLine));
+if ~isempty(p.Results.IntersectionLine) && intersectionPlot
+    h=plot(testX(:,[1:end 1])',testY(:,[1:end 1])',p.Results.IntersectionLine);
+    noLegend(h(1:end-1));
 end
-
 
 % ================== Ring Labels ===================== %
 % LLabelIndices indicates which lrings will have a label
@@ -283,7 +335,7 @@ end
 
 for n=1:numel(labelIndices)
     i=labelIndices(n);
-    text(x(i+1,floor(end*15/16)),y(i+1,floor(end*15/16)),sprintf('L*=%d',allLRings(i)),'Color',cols{n},'FontWeight','normal');
+    text(rings.x(i+1,floor(end*15/16)),rings.y(i+1,floor(end*15/16)),sprintf('L*=%d',allLRings(i)),'Color',cols{n},'FontWeight','normal');
 end
 
 % ================== Centre mark ===================== %
@@ -301,7 +353,7 @@ end
 
 % ================== Reference rings ===================== %
 %if a reference is supplied, add just the L*=100 line
-if ~isempty(refgamut) && strcmp(ringRef,'none')
+if ~isempty(refgamut) && ~intersectionPlot
     [xref,yref] = calcRings(refgamut,[]);
     plot(xref(end,[1:end 1]),yref(end,[1:end 1]),p.Results.RefLine);
 end
@@ -388,7 +440,7 @@ for n=1:max(nprims,nrefprims)
             %and the start
             if (ringorigin)
                 %from the ring
-                opt=[x(end,hue),y(end,hue)];
+                opt=[rings.x(end,hue),rings.y(end,hue)];
             else
                 %or from the origin
                 opt=[0,0];
@@ -411,7 +463,7 @@ axis(axis*1.05);
 %make the axes equal
 axis equal
 %add the title
-t=sprintf('CIELab gamut rings\n%s\nVolume = %g',gamut.title, vol);
+t=sprintf('CIELab gamut rings\n%s\nVolume = %g',gamut.title, testVol);
 title(t);
 %and the axis labels
 xlabel('a^*_{RSS}')
@@ -427,8 +479,8 @@ if ~isempty(p.Results.ScatterData)
   i = floor(LABu(:,1)/10);
   a = atan2(LABu(:,2),LABu(:,3));
   h = mod(a/pi*180+360,360);
-  rx = interp2(0:360,0:10,x(:,[end 1:end]),h,i);
-  ry = interp2(0:360,0:10,y(:,[end 1:end]),h,i);
+  rx = interp2(0:360,0:10,rings.x(:,[end 1:end]),h,i);
+  ry = interp2(0:360,0:10,rings.y(:,[end 1:end]),h,i);
   v = sum(LABu(:,2:3).^2,2)*dL*10*dH/2;
   bv = (rx.^2+ry.^2)*dH/2;
   r = (2*(bv+v)/dH).^0.5;
@@ -467,3 +519,62 @@ function noLegend(h)
         set(h,'HandleVisibility','off');
     end
 end
+
+function [rings] = ringsBase(gamut, LRings, ref)
+     dH=2*pi/gamut.hsteps;
+     dL=100/gamut.Lsteps;
+     %get the map of the volume in cylintrical coordinates
+     volmapFn = @(a) sum(a(:,1).*(a(:,2).^2)*dL*dH/2);
+     volmap=cellfun(volmapFn,gamut.cylmap);
+     if (nargin > 2)
+       refVolmap=cellfun(volmapFn,ref.cylmap);
+       volmap = max(volmap, refVolmap);
+     end
+     %Get the accumulated volume sum (the final row will be the total)
+     %and calculate the radius required to represent that volume
+     %adding a zero radius at the start
+     r2=interp1([0 1:100/gamut.Lsteps:100],[zeros(1,gamut.hsteps);(2*cumsum(volmap)/dH)],[0 LRings 100]);
+     rings = struct();
+     rings.r2 = r2;
+     rings.midH=dH/2:dH:2*pi;
+     rings.dH=dH;
+     rings.dL=dL;
+     rings.LRings=LRings;
+     rings.ux=sin(rings.midH);
+     rings.uy=cos(rings.midH);
+     r=sqrt(r2);
+     rings.x=r.*rings.ux;
+     rings.y=r.*rings.uy;
+     rings.vol=sum(volmap(:));
+end
+
+function [x,y,vol] = calcSubRings(rings, gamut)
+    volmap=cellfun(@(a) sum(a(:,1).*(a(:,2).^2)*rings.dL*rings.dH/2),gamut.cylmap);
+    ri2=interp1([0 1:100/gamut.Lsteps:100],[zeros(1,gamut.hsteps);(2*cumsum(volmap)/rings.dH)],[0 rings.LRings 100]);
+    rg2=rings.r2;
+    r=sqrt(min(rg2(2:end,:),(ri2(2:end,:)-ri2(1:end-1,:)+rg2(1:end-1,:))));
+    x=r.*rings.ux;
+    y=r.*rings.uy;
+    vol=sum(volmap(:));
+end
+
+function t = isTrue(v)
+    if islogical(v)
+      t = v;
+    else
+      t = false;
+    end
+end
+
+function values = normRange(range, N)
+  if numel(range)==1
+    values=repmat(range, 1, N);
+  else
+    values = interp1(normSteps(numel(range)),range,normSteps(N));
+  end
+end
+
+function range = normSteps(N)
+  range = (0:N-1)/(N-1);
+end
+
